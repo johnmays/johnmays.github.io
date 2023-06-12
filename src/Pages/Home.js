a8cb0b13fea044081b2864eaf1852dccb65d02cf
John Mays <25927827+johnmays@users.noreply.github.com>
2023-06-12 04:26:04 -0400

    few details added to homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,8 @@
 export default function Home() {
-    return <h1>Home</h1>
+    return(
+        <div className="summary-box">
+            <h1>John Mays</h1>
+            I am a software mason, a graphic designer, and a recent college graduate.
+        </div>
+    )
 }
