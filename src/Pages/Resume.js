87de9aa0957870b4498a1e594ef6f5ef1cd86ee8
John Mays <25927827+johnmays@users.noreply.github.com>
2023-06-13 18:43:25 -0400

    working on pdf display on resume page

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,3 +1,21 @@
+import { pdfjs, Document, Page } from 'react-pdf';
+
+// importing PDFjs worker.
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    // "pdfjs-dist/build/pdf.worker.min.js",
+    "react-pdf/node_modules/pdfjs-dist/build/pdf.worker.min.js",
+    import.meta.url
+  ).toString();
+
 export default function Resume() {
-    return <h1>Resume</h1>
+
+    return(
+        //style={{height: "75vh", width: "100vw", display: "flex", overflow: "hidden"}}
+        //"06_13_23_mays_resume.pdf"
+        <div className='pdf-box'>
+            <Document file="test_resume.pdf" onLoadError={console.error}> 
+                <Page pageNumber={1} renderTextLayer={false} scale={1.0} renderMode='svg'/>
+            </Document>
+        </div>
+    )
 }
