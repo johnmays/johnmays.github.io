9ad6cac2ab001759347e4617a0d1b95fb7d65c04
John Mays <25927827+johnmays@users.noreply.github.com>
2023-06-18 02:57:58 -0400

    getting somewhere; logical save with nav

--- src/Components/CenteredNavBar.js
+++ src/Components/CenteredNavBar.js
@@ -6,7 +6,7 @@ export default function CenteredNavBar() {
     const link_ref_4 = useRef(null);
     const [position_link_3, setPosition_3] = useState({ x: 0, y: 0 });
     const [position_link_4, setPosition_4] = useState({ x: 0, y: 0 });
-  
+    const [navBounds, setNavBounds] = useState([0,0,0,0,0])
     useEffect(() => {
       function handleResize() {
         const x_3 = link_ref_3.current.offsetLeft;
@@ -30,7 +30,7 @@ export default function CenteredNavBar() {
             <Link to='/about' className="nav-link">About</Link>
             <Link to='/resume' className="nav-link" ref={link_ref_3}>Resume</Link>
             <Link to='/designportfolio' className="nav-link" ref={link_ref_4}>Design_Portfolio</Link>
-            <div className="hover-selector"></div>
+            <div className="hover-selector" style={{left: "212px", right: "10px"}}></div>
         </nav>
         <div>3rd link: (x:{ position_link_3.x_3 }, y:{ position_link_3.y_3}); 4th link: (x:{ position_link_4.x_4 }, y:{ position_link_4.y_4})</div>
     </div>
