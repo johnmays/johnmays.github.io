81d616dc1f90459c64ec114b827533a4b48b9efb
John Mays <25927827+johnmays@users.noreply.github.com>
2023-06-18 00:40:01 -0400

    successful retrieval of element widths achieved

--- src/Components/CenteredNavBar.js
+++ src/Components/CenteredNavBar.js
@@ -1,11 +1,38 @@
+import { useRef, useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 export default function CenteredNavBar() {
-    return <nav className="navbar">
-        <Link to='/home' className="nav-link">Home</Link>
-        <Link to='/about' className="nav-link">About</Link>
-        <Link to='/resume' className="nav-link">Resume</Link>
-        <Link to='/designportfolio' className="nav-link">Design_Portfolio</Link>
-        <div className="hover-selector"></div>
-    </nav>
+    const link_ref_3 = useRef(null);
+    const link_ref_4 = useRef(null);
+    const [position_link_3, setPosition_3] = useState({ x: 0, y: 0 });
+    const [position_link_4, setPosition_4] = useState({ x: 0, y: 0 });
+  
+    useEffect(() => {
+      function handleResize() {
+        const x_3 = link_ref_3.current.offsetLeft;
+        const y_3 = link_ref_3.current.offsetTop;
+        setPosition_3({ x_3, y_3 });
+        const x_4 = link_ref_4.current.offsetLeft;
+        const y_4 = link_ref_4.current.offsetTop;
+        setPosition_4({ x_4, y_4 });
+      }
+  
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, 
+    [link_ref_3, link_ref_4]);
+
+    return (
+    <div>
+        <nav className="navbar">
+            <Link to='/home' className="nav-link">Home</Link>
+            <Link to='/about' className="nav-link">About</Link>
+            <Link to='/resume' className="nav-link" ref={link_ref_3}>Resume</Link>
+            <Link to='/designportfolio' className="nav-link" ref={link_ref_4}>Design_Portfolio</Link>
+            <div className="hover-selector"></div>
+        </nav>
+        <div>3rd link: (x:{ position_link_3.x_3 }, y:{ position_link_3.y_3}); 4th link: (x:{ position_link_4.x_4 }, y:{ position_link_4.y_4})</div>
+    </div>
+    )
 }
