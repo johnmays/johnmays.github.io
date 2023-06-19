eab9730677d0b4bf32bffc18fbdf6aac4225550a
John Mays <25927827+johnmays@users.noreply.github.com>
2023-06-18 21:36:15 -0400

    final working version for now; entrance exits & directional sliding all added

--- src/Components/CenteredNavBar.js
+++ src/Components/CenteredNavBar.js
@@ -8,7 +8,9 @@ export default function CenteredNavBar() {
     const link_ref_4 = useRef(null);
     const [navBounds, setNavBounds] = useState([0,0,0,0,0]);
     const [selectorBounds, setSelectorBounds] = useState([0,0]);
-    //const [linkSelected, setLinkSelected] = useState(0);
+    const [transitionStyle, setTransitionStyle] = useState("opacity 0.3s");
+    const [firstDisplay, setFirstDisplay] = useState(true);
+    const [prevLinkNumber, setPrevLinkNumber] = useState(0);
 
     useEffect(() => {
       function handleResize() {
@@ -26,7 +28,7 @@ export default function CenteredNavBar() {
     }, 
     [link_ref_1, link_ref_2, link_ref_3, link_ref_4]);
 
-    function handleHover(linkNumber) {
+    function handleLinkHover(linkNumber) {
       if (linkNumber === 1){
         setSelectorBounds([navBounds[0],navBounds[4]-navBounds[1]]);
       }else if (linkNumber === 2){
@@ -36,20 +38,33 @@ export default function CenteredNavBar() {
       }else if (linkNumber === 4){
         setSelectorBounds([navBounds[3],0]);
       }else{
-        console.log("Invalid link number passed...")
+        console.log("Invalid link number passed...");
       }
+
+      if (firstDisplay) {
+        setTransitionStyle("opacity 0.3s");
+        setFirstDisplay(false);
+      }else{
+        if (prevLinkNumber > linkNumber){
+          setTransitionStyle("opacity 0.3s, left 0.3s, right 0.3s 0.1s");
+        }else{
+          setTransitionStyle("opacity 0.3s, left 0.3s 0.1s, right 0.3s");
+        }
+      }
+      setPrevLinkNumber(linkNumber);
     };
 
+    function handleNavHover(){
+      setFirstDisplay(true);
+    }
+
     return (
-    <div>
-        <nav className="navbar">
-            <Link to='/home' className="nav-link" ref={link_ref_1} onMouseEnter={() => handleHover(1)}>Home</Link>
-            <Link to='/about' className="nav-link" ref={link_ref_2} onMouseEnter={() => handleHover(2)}>About</Link>
-            <Link to='/resume' className="nav-link" ref={link_ref_3} onMouseEnter={() => handleHover(3)}>Resume</Link>
-            <Link to='/designportfolio' className="nav-link" ref={link_ref_4} onMouseEnter={() => handleHover(4)}>Portfolio</Link>
-            <div className="hover-selector" style={{left: `${selectorBounds[0]}px`, right: `${10+selectorBounds[1]}px`}}></div>
-        </nav>
-        <div>{navBounds[0]},{navBounds[1]},{navBounds[2]},{navBounds[3]},{navBounds[4]}</div>
-    </div>
+      <nav className="navbar" onMouseEnter={() => handleNavHover()}>
+          <Link to='/home' className="nav-link" ref={link_ref_1} onMouseEnter={() => handleLinkHover(1)}>Home</Link>
+          <Link to='/about' className="nav-link" ref={link_ref_2} onMouseEnter={() => handleLinkHover(2)}>About</Link>
+          <Link to='/resume' className="nav-link" ref={link_ref_3} onMouseEnter={() => handleLinkHover(3)}>Resume</Link>
+          <Link to='/designportfolio' className="nav-link" ref={link_ref_4} onMouseEnter={() => handleLinkHover(4)}>Portfolio</Link>
+          <div className="hover-selector" style={{left: `${selectorBounds[0]}px`, right: `${10+selectorBounds[1]}px`, transition: `${transitionStyle}`}}></div>
+      </nav>
     )
 }
