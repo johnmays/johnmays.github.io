ada661aa2389a8792bf8aef1b13601aa44d2c414
John Mays <25927827+johnmays@users.noreply.github.com>
2023-06-21 00:42:48 -0400

    color-changing svg added; will try to make modular now

--- src/Pages/Profiles.js
+++ src/Pages/Profiles.js
@@ -2,6 +2,7 @@ import linkedin_logo from '../images/linkedin_logo.svg'
 import github_logo from '../images/github_logo.svg'
 import fiverr_logo from '../images/fiverr_logo.svg'
 import ProfileBox from '../Components/ProfileBox'
+import smartGithubLogo from '../Components/smartGithubLogo'
 
 export default function Profiles() {
     return(
@@ -11,6 +12,7 @@ export default function Profiles() {
                 {ProfileBox(linkedin_logo, "LinkedIn", "boo")}
                 {ProfileBox(fiverr_logo, "Fiverr", "boo")}
             </div>
+            {smartGithubLogo("#00ff00")}
         </div>
     )
 
