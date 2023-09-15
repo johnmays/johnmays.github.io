d024ea8917e67124045c71a311b4552a94712038
John Mays <25927827+johnmays@users.noreply.github.com>
2023-09-15 06:23:34 -0400

    added download button to resume page

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -13,11 +13,17 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 export default function Resume() {
 
     return(
-        <div>
+        <div className='pdf-box'>
             <div className='pdf-window'>
-                <Document file="06_13_23_mays_resume.pdf" onLoadError={console.error}> 
+                <Document file="09_15_23_mays_online_resume.pdf" onLoadError={console.error}> 
                     <Page pageNumber={1}  devicePixelRatio={2.0} renderTextLayer={false}/>
                 </Document>
+            </div>
+            <div className='button-box'>
+                <a href="/09_15_23_mays_online_resume.pdf" target="_blank" rel="noopener noreferrer">
+                    <button>Download</button>
+                        <button>Download</button>
+                </a>
             </div>
         </div>
     )
