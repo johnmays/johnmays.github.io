d5ed29cb045a658f43ba170c5c3c4c1638d8411e
John Mays <25927827+johnmays@users.noreply.github.com>
2023-09-15 03:24:56 -0400

    small content changes post-hiatus

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ export default function About() {
     return(
         <div className="content-box">
             <h1>About Me</h1>
-            <p>Hi, my name is John. I'm 22 years old & I'm from Cincinnati, Ohio. I recently graduated college with a B.S. in computer science and I'm currently looking for work. My concentration was artificial intelligence, but I also studied software development, numerical linear algebra (for image processing), and the ethics of technology.  My extra-academic interests include UI design, graphic design, and boxing. I was a part-time graphic designer for four years and I still do freelance work. Right now, I'm learning to speak modern Greek.  I can be reached at john dot k dot mays at-symbol outlook dot com, or feel free to look at one of <Link to='/profiles' className="text-link">my professional profiles</Link>.</p>
+            <p>Hi, my name is John. I'm 22 years old & I'm from Cincinnati, Ohio. I recently graduated college with a B.S. in computer science and I'm currently looking for work. My concentration was artificial intelligence, but I also studied software development, numerical linear algebra (mainly for image processing), and the ethics of AI.  My other interests include UI design, graphic design, and boxing. I was a part-time graphic designer for four years and I still do freelance work. I can be reached at john dot k dot mays at-symbol outlook dot com, or feel free to look at one of <Link to='/profiles' className="text-link">my professional profiles</Link>.</p>
         </div>
     )
 }
--- src/Pages/WebsiteInformation.js
+++ src/Pages/WebsiteInformation.js
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom"
 export default function WebsiteInformation() {
     return(
         <div className="content-box">
             <h1>About this website</h1>
-            <p>hi</p>
+            <p>This website was created using ReactJS.  It is hosted through github pages and all of its source code can be found <Link to='https://github.com/johnmays/johnmays.github.io' className="text-link">here</Link>.  Depending on the time you visit the website, I am using one of two color palettes: Either nearly monotone (#fafafe, #1a191f, #26252d) or somewhat colorful (#017dbb, #bc955c, #202720).  See if you can find any of the easter eggs.</p>
         </div>
     )
 }
