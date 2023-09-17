df7a2a7cc34f66a1af5a6a9a0c8bad1d272a2570
John Mays <25927827+johnmays@users.noreply.github.com>
2023-09-17 18:15:40 -0400

    more mobile formatting

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -4,7 +4,7 @@ import guts from '../assets/images/guts.jpg'
 export default function NotFound() {
     return(
         <div className='content-box not-found-box'>
-            <h1>404: The page you are looking for does not exist or is missing.</h1>
+            <h3>404: The page you are looking for does not exist or is missing.</h3>
             <img src={guts} alt="panel from the comic Berserk of the character Guts frowning"></img>
         </div>
     )
