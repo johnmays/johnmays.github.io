719c939ca4d588415ece1fcb7a9bc0ad686599f9
John Mays <25927827+johnmays@users.noreply.github.com>
2023-09-20 21:35:19 -0400

    a few wording changes + a styling change to keep profile logos apart on mobile

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
     return(
         <div className="content-box">
             <h1>About Me</h1>
-            <p>Hi, my name is John. I'm {age} years old & I'm from Cincinnati, Ohio. I recently graduated college with a B.S. in computer science and I'm currently looking for work. My concentration was artificial intelligence, but I also studied software development, numerical linear algebra (mainly for image processing), and the ethics of AI.  My other interests include UI design, graphic design, and boxing. I was a part-time graphic designer for four years and I still do freelance work. I can be reached at john dot k dot mays at-symbol outlook dot com, and feel free to look at any of <Link to='/profiles' className="text-link">my professional profiles</Link>.</p>
+            <p>Hi, my name is John. I'm {age} years old & I'm from Cincinnati, Ohio. I recently graduated college with a B.S. in computer science and I'm currently looking for work. My concentration was in artificial intelligence, but I also studied software development, numerical linear algebra (mainly for image processing), and the ethics of AI.  My other interests include UI design, graphic design, and boxing. I was a part-time graphic designer for four years and I still do freelance work. I can be reached at john dot k dot mays at-symbol outlook dot com, and feel free to look at any of <Link to='/profiles' className="text-link">my professional profiles</Link>.</p>
         </div>
     )
 }
