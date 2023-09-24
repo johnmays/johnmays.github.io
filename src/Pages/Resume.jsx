816602be4f78690eff2acc884ffad42ef2bb3cde
John Mays <25927827+johnmays@users.noreply.github.com>
2023-09-24 04:06:08 -0400

    new resume

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import resume_image from '../assets/images/09_17_23_mays_online_resume.jpg'
+import resume_image from '../assets/images/09_24_23_mays_online_resume.jpg'
 export default function Resume() {
 
     return(
         <div className='resume-content'>
-            <a href="/09_17_23_mays_online_resume.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="/09_24_23_mays_online_resume.pdf" target="_blank" rel="noopener noreferrer">
                 <img src={resume_image} alt="My resume.  Text can be extracted by actually downloading it as a pdf with the button below."></img>
             </a>
-            <a href="/09_17_23_mays_online_resume.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="/09_24_23_mays_online_resume.pdf" target="_blank" rel="noopener noreferrer">
                 <button>Download</button>
             </a>
         </div>
